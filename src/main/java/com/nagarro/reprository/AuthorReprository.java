e72282ad6a342fd88149e89ca8b23b705c3a15e1
Prem <gpremp7736@gmail.com>
2023-02-20 15:25:53 +0530

    Azure SpringBoot App

---
+++ src/main/java/com/nagarro/reprository/AuthorReprository.java
@@ -0,0 +1,23 @@
+package com.nagarro.reprository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.nagarro.entities.Author;
+
+
+
+
+
+/**
+ *  Repository interface of Author to perform database manipulation of Author details
+ * @author premgupta
+ *
+ */
+public interface AuthorReprository extends CrudRepository<Author, Integer>{
+	
+	public List<Author> findByFirstNameAndLastName(final String firstName,final String lastName);
+	public List<Author> deleteByFirstNameAndLastName(final String firstName,final String lastName);
+
+}
